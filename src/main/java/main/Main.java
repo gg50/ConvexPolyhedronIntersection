89e3f2037410a4c525879271de5d1bb4d5256249
Georgi Gospodinov <gg50@st-andrews.ac.uk>
2019-11-07 17:41:24 +0000

    Intersections (clippings) seem to work and make sense. I still need to visualize all of the figures to be sure.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,8 +15,15 @@ public class Main {
         log.info("box2 = {}", box2);
         Polyhedron tetrahedron1 = createTetrahedron1();
         log.info("tetrahedron1 = {}", tetrahedron1);
-        Polyhedron clip = box1.clip(box2);
-        log.info("clip = {}", clip);
+        Polyhedron tetrahedron2 = createTetrahedron2();
+        log.info("tetrahedron1 = {}", tetrahedron2);
+
+        log.info("box1.clip(box2) = {}", box1.clip(box2));
+        log.info("box1.clip(tetrahedron1) = {}", box1.clip(tetrahedron1));
+        log.info("box1.clip(tetrahedron2) = {}", box1.clip(tetrahedron2));
+        log.info("box2.clip(tetrahedron1) = {}", box2.clip(tetrahedron1));
+        log.info("box2.clip(tetrahedron2) = {}", box2.clip(tetrahedron2));
+        log.info("tetrahedron1.clip(tetrahedron2) = {}", tetrahedron1.clip(tetrahedron2));
     }
 
     private static Polyhedron createCube1() {
@@ -30,18 +37,28 @@ public class Main {
 
     private static Polyhedron createCube2() {
         log.traceEntry("()");
-        Vector3 p3 = new Vector3(1f, 1f, 1f);
-        float v = 10f;
-        Vector3 p4 = new Vector3(v, v, v);
+        float v1 = 0f;
+        Vector3 p3 = new Vector3(v1, v1, v1);
+        float v2 = 10f;
+        Vector3 p4 = new Vector3(v2, v2, v2);
         return Box.create(p3, p4);
     }
 
     private static Polyhedron createTetrahedron1() {
         log.traceEntry("()");
-        Vector3 v1 = new Vector3(0f, 0f, -10f);
-        Vector3 v2 = new Vector3(10f, 0f, 0f);
-        Vector3 v3 = new Vector3(-10f, 0f, 0f);
-        Vector3 v4 = new Vector3(0f, 10f, -5f);
+        Vector3 v1 = new Vector3(0f, 0f, -5f);
+        Vector3 v2 = new Vector3(10f, 0f, 5f);
+        Vector3 v3 = new Vector3(-10f, 0f, 5f);
+        Vector3 v4 = new Vector3(0f, 10f, 0f);
+        return Tetrahedron.create(v1, v2, v3, v4);
+    }
+
+    private static Polyhedron createTetrahedron2() {
+        log.traceEntry("()");
+        Vector3 v1 = new Vector3(0f, 10f, 5f);
+        Vector3 v2 = new Vector3(10f, 10f, -5f);
+        Vector3 v3 = new Vector3(-10f, 10f, -5f);
+        Vector3 v4 = new Vector3(0f, 0f, 0f);
         return Tetrahedron.create(v1, v2, v3, v4);
     }
 }
